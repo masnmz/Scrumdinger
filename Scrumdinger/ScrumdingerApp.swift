0d3956ba2398eca5dee113f94efdeac44be20f9c
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2025-01-15 13:01:23 +0000

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Mehmet Alp Sönmez on 15/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
